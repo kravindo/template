ca1335d61d9c0ab621956a4411e1bc5acf665797
FgDevLab <tatamf.id@gmail.com>
2024-09-21 02:04:53 +0700

    fix

--- components/layout.js
+++ components/layout.js
@@ -2,24 +2,23 @@ import Image from "next/image";
 
 function Layout({ children, companyName, companyLogo, companyEmail, customerEmail, companyDomain }) {
     return (
-        <div className="bg-[#f3f7f9] min-h-screen w-full">
-            <div className="w-[40rem] flex flex-col gap-8 bg-white mx-auto p-8 border">
-                <div>
-                    <Image className="rounded-full" width={70} height={70} src={companyLogo} alt="Company Logo" style={{ maxWidth: '100%' }} />
+        <div className="bg-[#f1f2f3] min-h-screen w-full">
+            <div className="w-full max-w-[40rem] flex flex-col bg-white mx-auto border">
+                <div className="w-full flex flex-col h-[4.5rem] px-8 items-center justify-center border-b">
+                    <Image className="rounded-full" width={40} height={40} src={companyLogo} alt="Company Logo" style={{ maxWidth: '100%' }} />
                 </div>
-                <div className="flex flex-col gap-5 w-full leading-relaxed">
+                <div className="text-sm flex flex-col p-6 gap-4 w-full leading-relaxed">
                     {children}
+                    <div className="flex flex-col mt-2 gap-2">
+                        <span className="text-base text-blue-700">Tim Kravindo.com</span>
+                        <span className="text-xs leading-relaxed">Jika Anda ingin berbicara dengan anggota tim dukungan, <br /> mohon hubungi <span className="text-blue-700">support@kravindo.com</span> .</span>
+                    </div>
                 </div>
             </div>
-            <div className="w-[40rem] leading-loose gap-3 items-center px-10 py-6 flex flex-col text-center mx-auto text-[#666666]">
+            <div className="w-full max-w-[40rem] leading-loose gap-3 items-center px-10 py-6 flex flex-col text-center mx-auto text-[#666666]">
                 <span className="text-xs">Email ini ditujukan untuk <span className="text-blue-700 cursor-pointer">{customerEmail}</span></span>
                 <span className="text-xs">Harap waspada terhadap upaya penipuan. Kami tidak akan pernah meminta informasi pribadi Anda, termasuk kata sandi, melalui email atau komunikasi lain yang tidak diminta.</span>
                 <span className="text-xs">Kami selalu berusaha untuk memberikan informasi yang paling akurat dan terkini. Apabila terdapat perbedaan informasi, kami sarankan untuk mengunjungi website <span className="text-blue-700 underline cursor-pointer">{companyDomain}</span> untuk mendapatkan informasi terbaru.</span>
-            </div>
-            <div className="w-[40rem] border-t gap-6 items-center px-10 py-6 flex flex-row justify-center mx-auto text-[#666666]">
-                <span className="text-xs cursor-pointer">Hubungi Kami</span>
-                <span className="text-xs cursor-pointer">Syarat & Ketentuan</span>
-                <span className="text-xs cursor-pointer">Kebijakan Privasi</span>
             </div>
         </div>
     );
